288f20017e96b62b90d788f2d98e1b7cd35f2fe9
xodlf8044 <xodlf8044@gmail.com>
2020-12-03 16:53:23 +0900

    finish react-redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainers';
+function App() {
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <TodosContainer />
+    </div>
+  );
+}
+
+export default App;
